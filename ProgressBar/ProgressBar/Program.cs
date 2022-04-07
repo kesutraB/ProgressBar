77bfeb8b339372224c6ee31a0960ed4710644f7f
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-04-07 20:27:01 +0200

    first type of progress bar

---
+++ ProgressBar/ProgressBar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+namespace ProgressBar
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = Encoding.UTF8;
+			Console.CursorVisible = false;
+
+			//first type
+			Random random = new Random();
+			int x = random.Next(1, 25);
+			int y = random.Next(200, 500);
+			for (int i = 0; i < x; i++)
+			{
+				Console.Write($"{i + 1}. step: ");
+				Thread.Sleep(y);
+				for (int j = 0; j < (i + 1); j++)
+				{
+					Console.Write("\u2593");
+				}
+				Console.Write($" {i + 1}/{x} steps\n");
+
+				//second type
+			}
+		}
+	}
+}
