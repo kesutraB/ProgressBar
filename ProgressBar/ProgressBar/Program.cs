7285d72e5e2b6eb6b1358d41b759e4f23c6b7713
kesutraB <99909890+kesutraB@users.noreply.github.com>
2022-04-09 16:14:41 +0200

    randomized progress bar one

--- ProgressBar/ProgressBar/Program.cs
+++ ProgressBar/ProgressBar/Program.cs
@@ -6,45 +6,92 @@ namespace ProgressBar
 {
 	internal class Program
 	{
+		public const string divone = "\u2591";
+		public const string divtwo = "\u2592";
+		public const string divthree = "\u2593";
+		public const int MaxX = 25;
+		public const int MaxY = 500;
+		public const int steps = 50;
+		public const int delay = 100;
+
 		static void Main(string[] args)
 		{
 			Console.OutputEncoding = Encoding.UTF8;
 			Console.CursorVisible = false;
 
 			//first type
-			StepsAndDelayGeneration(out int x, out int y);
-			PrintInfo(x, y);
+			//ProgressBarTypeOne();
+			Console.WriteLine();
 
 			//second type
+			ProgressBarTypeTwo();
+			
 		}
 
-		#region printing
+		private static void ProgressBarTypeOne()
+		{
+			RandomSteps(out int x);
+			RandomDelay(out int y);
+			PrintInfo(x, y);
+		}
+
+		private static void ProgressBarTypeTwo()
+		{
+			PrintCharacter(steps, divone);
+			for (int i = 0; i < steps; i++)
+			{
+				Console.CursorLeft = steps + 1;
+				PrintSteps(i, steps);
+			}
+		}
+
+		#region ProgressBar type one
 
 		private static void PrintInfo(int x, int y)
 		{
 			for (int i = 0; i < x; i++)
 			{
-				Console.Write($"{i + 1}. step: ");
-				Thread.Sleep(y);
 				PrintProgressBar(i);
-
-				Console.Write($" {i + 1}/{x} steps\n");
+				PrintSteps(i, x);
+				Thread.Sleep(y);
 			}
 		}
 
 		private static void PrintProgressBar(int i)
 		{
-			for (int j = 0; j < (i + 1); j++)
-				Console.Write("\u2593");
+			Console.CursorLeft = i;
+			Console.Write(divthree);
+		}
+
+		private static void RandomSteps(out int x)
+		{
+			Random random = new Random();
+			x = random.Next(MaxX);
+		}
+
+		private static void RandomDelay(out int y)
+		{
+			Random random = new Random();
+			y = random.Next(MaxY);
 		}
 
 		#endregion
 
-		private static void StepsAndDelayGeneration(out int x, out int y)
+		#region UniversalPrinting
+
+		private static void PrintSteps(int i, int stepcount)
 		{
-			Random random = new Random();
-			x = random.Next(1, 25);
-			y = random.Next(200, 500);
+			Console.Write($"{i + 1}/{stepcount}");
 		}
+
+		private static void PrintCharacter(int charcount, string character)
+		{
+			for (int i = 0; i < charcount; i++)
+			{
+				Console.Write(character);
+			}
+		}
+
+		#endregion
 	}
 }
